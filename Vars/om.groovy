ee66a9414869ad3c7e2a4ef59aaaa53089281b1b
om <omejjigiri99@gmail.com>
2024-11-06 22:47:04 +0530

    c

--- Vars/om.groovy
+++ Vars/om.groovy
@@ -1,4 +1,31 @@
-@Library('Devops_job-library') _
+def call () {
+    pipeline {
+        agent any 
+        stages {
+            stage('compile'){
+                steps{
+                    echo 'compile'
+                }
+            }
 
-om()
+            stage('testcases'){
+                steps{
+                    echo 'testcases'
+                }
+            }  
 
+
+            stage('build'){
+                steps{
+                    echo 'build'
+                }
+            }  
+
+            stage('release'){
+                steps{
+                    echo 'release'
+                }
+            }    
+        }
+    }
+}
