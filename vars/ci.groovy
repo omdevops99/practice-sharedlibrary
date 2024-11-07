251afa54f5ca30a474221d61ba35058c0d2e6279
om <omejjigiri99@gmail.com>
2024-11-07 21:39:15 +0530

    c

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,31 +1,45 @@
 def call() {
     node {
+        // Print all environment variables
         sh 'env'
-        if(env.TAG_NAME ==~ ".*") {
-           env.branchName = env.TAG_NAME
-        }  else {
-           env.branchName = env.BRANCH_NAME     
-           }
-        { 
-          stage('code checkout'){
-                git branch: env.branchName, url: 'https://github.com/omdevops99/expense_jenkins.git'
-            }
-            stage('build') {}
-        } else if(env.BRANCH_NAME ==~ "PR.*") {
-          stage('test') {}
-          stage('Release') {}
-        } else if(env.TAG_NAME ==~ ".*") {
-          stage('compile'){}
-          stage('test') {}
+
+        // Determine branchName based on TAG_NAME or BRANCH_NAME
+        if (env.TAG_NAME ==~ ".*") {
+            env.branchName = env.TAG_NAME
         } else {
-          stage('compile'){}
-          stage('test') {}
-          stage('Release') {}
+            env.branchName = env.BRANCH_NAME
         }
+
+        // Code checkout stage
+        stage('code checkout') {
+            git branch: env.branchName, url: 'https://github.com/omdevops99/expense_jenkins.git'
+        }
+
+        // Conditional stages based on BRANCH_NAME and TAG_NAME
+        if (env.BRANCH_NAME ==~ "PR.*") {
+            stage('test') {
+                echo 'Running tests for PR branch...'
+            }
+            stage('Release') {
+                echo 'Releasing from PR branch...'
+            }
+        } else if (env.TAG_NAME ==~ ".*") {
+            stage('compile') {
+                echo 'Compiling code for tag...'
+            }
+            stage('test') {
+                echo 'Running tests for tag...'
+            }
+        } else {
+            stage('compile') {
+                echo 'Compiling code...'
+            }
+            stage('test') {
+                echo 'Running tests...'
+            }
+            stage('Release') {
+                echo 'Releasing code...'
+            }
         }
     }
-
-    ////
-
-
-
+}
