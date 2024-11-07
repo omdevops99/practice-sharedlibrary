ff65ccf94ef3da19a59ff483e14ed82cde20a9ee
om <omejjigiri99@gmail.com>
2024-11-07 18:15:12 +0530

    c

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,36 +1,32 @@
 def call() {
-  pipeline {
-    agent any 
-    stages{
-        stage('compile'){
-            steps{
-                echo 'compile'
+    pipeline {
+        agent any 
+        stages {
+            stage('compile') {
+                steps {
+                    echo 'compile'
+                }
+            }
+            stage('test') {
+                steps {
+                    echo 'test'
+                }
+            }
+            stage('build') {
+                steps {
+                    echo 'build'
+                }
+            }
+            stage('integration test') {
+                steps {
+                    echo 'integration tests'
+                }
+            }
+            stage('Release') {
+                steps {
+                    echo 'Release'
+                }
             }
         }
     }
-    stage('test'){
-            steps{
-                echo 'test'
-            }
-        }
-
-    stage('build'){
-            steps{
-                echo 'build'
-            }
-        }    
-
-    stage('integration test'){
-            steps{
-                echo 'integration tests'
-            }
-        }    
-
-    stage('Release'){
-            steps{
-                echo 'Release'
-            }
-        }        
-  }
-
 }
